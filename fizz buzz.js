4b8f4c3479a3a33f5a1f65ae2ae769e79ceb586d
Maxime B <maxime.bourdin.tic@gmail.com>
2023-07-13 23:06:09 +0200

    Create fizz buzz.js

---
+++ fizz buzz.js
@@ -0,0 +1,47 @@
+
+function fizzBuzz(n, optimized) {
+    if (!optimized) {
+        for (let i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                print("FizzBuzz ");
+            } else if (i % 5 == 0) {
+                print("Buzz ");
+            } else if (i % 3 == 0) {
+                print("Fizz");
+            } else {
+                print(i);
+            }
+        }
+    } else {
+        let s = "";
+        let m3 = 0;
+        let m5 = 0;
+        
+        for (let i = 1; i <= n; i++) {
+            m3++; m5++;
+            
+            if (m3 == 3) {
+                s += "Fizz";
+                m3 = 0;
+            }
+            if (m5 == 5) {
+                s += "Buzz";
+                m5 = 0;
+            }
+            
+            // if (s != "") {
+            //     s += " ";
+            // }
+            
+            if (s == "") {
+                print(i);
+            } else {
+                print(s);
+            }
+            
+            s = "";
+        }
+    }
+}
+
+fizzBuzz(100, true);
